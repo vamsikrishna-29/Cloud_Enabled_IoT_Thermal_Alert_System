854bd7fd5319a2555f4915e621fbb0b1dbf20a5a
KRISTIPATI VAMSI KRISHNA <vamsikrishnareddy029@gmail.com>
2026-08-05 12:56:22 +0530

    Delete INTERRUPTS directory

--- INTERRUPTS/eint0.h
+++
@@ -1,5 +0,0 @@
-#include "types.h"
-void Init_eint1(void);
-void Init_eint2(void);
-void eint1_isr(void) __irq;
-void eint2_isr(void) __irq;
--- INTERRUPTS/eint0_defines.h
+++
@@ -1,2 +0,0 @@
-#define EINT1_VIC_CHNO 15
-#define EINT2_VIC_CHNO 16
