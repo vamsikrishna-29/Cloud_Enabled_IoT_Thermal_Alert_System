771fe4a1a418d774960f835da6ad4f4fce08bf42
KRISTIPATI VAMSI KRISHNA <vamsikrishnareddy029@gmail.com>
2026-08-05 13:01:25 +0530

    Add files via upload

---
+++ pin_function_defines.h
@@ -0,0 +1,31 @@
+//pin_function_defines.h
+#define PINFUNC1 0
+#define PINFUNC2 1
+#define PINFUNC3 2
+#define PINFUNC4 3
+
+//defines for external interrupt input pin function select
+
+#define EINT0_0_1 PINFUNC4
+
+#define EINT1_0_3 PINFUNC4
+
+#define EINT2_0_7 PINFUNC4
+
+
+//defines for i2c pin function select
+
+#define SCL_PIN_0_2   PINFUNC2 
+
+#define SDA_PIN_0_3   PINFUNC2 
+
+
+//defines for SPI Pin Function Select 
+
+#define SCK_0_4     PIN_FUNC2        //P0.4 
+
+#define MISO_0_5    PIN_FUNC2        //P0.5 
+
+#define MOSI_0_6    PIN_FUNC2        //P0.6   
+
+#define CS_0_7      7                //p0.7 
