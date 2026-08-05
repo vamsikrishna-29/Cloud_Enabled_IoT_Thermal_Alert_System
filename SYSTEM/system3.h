d14513bfb49914020f053628a8fe6de2abbfe9ba
KRISTIPATI VAMSI KRISHNA <vamsikrishnareddy029@gmail.com>
2026-08-05 12:49:46 +0530

    Delete SYSTEM directory

--- SYSTEM/system3.h
+++
@@ -1,9 +0,0 @@
-void System_Init(void);
-void Display_System_Status(void);
-void Monitor_Alarm(void);
-void Update_Local_SetPoint(void);
-void Upload_Temperature_To_Cloud(void);
-void Update_SetPoint_From_Cloud(void);
-void Load_SetPoint_From_EEPROM(void);
-
-
