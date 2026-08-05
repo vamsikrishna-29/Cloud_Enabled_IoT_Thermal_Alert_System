b3e77be7c88d40c54401ebf5ca21e7b41073e8fc
KRISTIPATI VAMSI KRISHNA <vamsikrishnareddy029@gmail.com>
2026-08-05 12:54:15 +0530

    Add files via upload

---
+++ i2c_peripheral.h
@@ -0,0 +1,17 @@
+//i2c_peripheral.h
+
+#include "types.h"
+
+void Init_I2C(void);
+
+void i2c_start(void);
+
+void i2c_restart(void);
+
+void i2c_write(u8 dat);
+
+void i2c_stop(void);
+
+u8 i2c_nack(void); 
+
+u8 i2c_mack(void);
